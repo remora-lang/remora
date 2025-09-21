8cdffb98e340eaef9a7bf46d7019f578d15b0828
zfnmxt <zfnmxt@zfnmxt.com>
2025-09-21 18:59:23 -0400

    Add `Pretty` instances.

--- src/Syntax.hs
+++ src/Syntax.hs
@@ -1,5 +1,7 @@
 module Syntax where
 
+import Data.Proxy
+import Prettyprinter
 import Text.Megaparsec.Pos (SourcePos)
 
 data Idx v
@@ -9,15 +11,32 @@ data Idx v
   | Add [Idx v] SourcePos
   | Concat [Idx v] SourcePos
 
+deriving instance (Show v) => Show (Idx v)
+
+instance (Show v, Pretty v) => Pretty (Idx v) where
+  pretty (IdxVar v _) = pretty v
+  pretty (Dim d _) = pretty d
+  pretty (Shape is _) = parens $ hsep ("shape" : map pretty is)
+  pretty (Add is _) = parens $ hsep ("+" : map pretty is)
+  pretty (Concat is _) = parens $ hsep ("++" : map pretty is)
+
 data Kind
   = KindArray
   | KindAtom
   deriving (Show, Eq, Ord)
 
+instance Pretty Kind where
+  pretty KindArray = "Array"
+  pretty KindAtom = "Atom"
+
 data Sort
   = SortShape
   | SortDim
   deriving (Show, Eq, Ord)
+
+instance Pretty Sort where
+  pretty SortShape = "Shape"
+  pretty SortDim = "Dim"
 
 data Atom f v
   = BoolVal Bool SourcePos
@@ -28,11 +47,65 @@ data Atom f v
   | ILambda [(v, Maybe Sort)] (Exp f v) SourcePos
   | Box [Idx v] (Exp f v) (Maybe (Type v)) SourcePos
 
+deriving instance (Show v) => Show (Atom Proxy v)
+
+instance (Show v, Pretty v) => Pretty (Atom Proxy v) where
+  pretty (BoolVal True _) = "#t"
+  pretty (BoolVal False _) = "#f"
+  pretty (IntVal i _) = pretty i
+  pretty (FloatVal f _) = pretty f
+  pretty (Lambda args e _) =
+    let pArgs =
+          parens $
+            hsep $
+              map
+                ( \(v, mt) ->
+                    case mt of
+                      Nothing -> pretty v
+                      Just t -> tupled $ [pretty v, pretty t]
+                )
+                args
+     in parens $ "λ" <+> pArgs <+> pretty e
+  pretty (TLambda args e _) =
+    let pArgs =
+          parens $
+            hsep $
+              map
+                ( \(v, mk) ->
+                    case mk of
+                      Nothing -> pretty v
+                      Just k -> tupled $ [pretty v, pretty k]
+                )
+                args
+     in parens $ "Tλ" <+> pArgs <+> pretty e
+  pretty (ILambda args e _) =
+    let pArgs =
+          parens $
+            hsep $
+              map
+                ( \(v, ms) ->
+                    case ms of
+                      Nothing -> pretty v
+                      Just s -> tupled $ [pretty v, pretty s]
+                )
+                args
+     in parens $ "Iλ" <+> pArgs <+> pretty e
+  pretty (Box is e mt _) =
+    parens $ "box" <+> hsep (map pretty is) <+> pretty e <+> maybe "" pretty mt
+
 data Type v
   = TVar v
   | Bool
   | Int
   | Float
+
+deriving instance (Show v) => Show (Type v)
+
+instance (Show v, Pretty v) => Pretty (Type v) where
+  pretty (TVar v) = pretty v
+  pretty Bool = "Bool"
+  pretty Int = "Int"
+  pretty Float = "Float"
 
 data Exp f v
   = Var v SourcePos
@@ -44,3 +117,28 @@ data Exp f v
   | TApp (Exp f v) [Type v] SourcePos
   | IApp (Exp f v) [Idx v] SourcePos
   | Unbox [v] (Exp f v) (Exp f v) SourcePos
+
+deriving instance (Show v) => Show (Exp Proxy v)
+
+instance (Show v, Pretty v) => Pretty (Exp Proxy v) where
+  pretty (Var v _) = pretty v
+  pretty (Array shape as _) =
+    group $
+      parens $
+        "Array"
+          <+> (parens $ hsep (map pretty shape))
+          <+> (group $ encloseSep "[" "]" ("," <> line) (map pretty as))
+  pretty (Frame shape es _) =
+    group $
+      parens $
+        "Array"
+          <+> (parens $ hsep (map pretty shape))
+          <+> (group $ encloseSep "[" "]" ("," <> line) (map pretty es))
+  pretty (App es _) =
+    parens $ hsep $ map pretty es
+  pretty (TApp e ts _) =
+    parens $ "t-app" <+> pretty e <+> hsep (map pretty ts)
+  pretty (IApp e is _) =
+    parens $ "i-app" <+> pretty e <+> hsep (map pretty is)
+  pretty (Unbox vs e b _) =
+    parens $ "unbox" <+> (parens (hsep (map pretty vs ++ [pretty e]))) <+> pretty b
