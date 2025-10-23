257bee05b1d257034286543b5588d1d01b1b1a5e
zfnmxt <zfnmxt@zfnmxt.com>
2025-10-22 22:21:36 -0400

    Clean-up.

--- src/CLI.hs
+++ src/CLI.hs
@@ -9,17 +9,14 @@ import Data.Text (Text)
 import Data.Text qualified as T
 import Data.Text.IO qualified as T
 import Interpreter qualified
-import Interpreter.Value (Val)
 import Parser qualified
-import Prettyprinter
 import SExp
-import Syntax
 import System.Console.CmdArgs
 import TypeCheck
 import Util
 import Prelude hiding (exp)
 
-data Remora
+data RemoraMode
   = REPL
   | Interpret
       { file :: Maybe FilePath,
@@ -32,6 +29,7 @@ data Remora
       }
   deriving (Data, Typeable, Show, Eq)
 
+parse :: RemoraMode
 parse =
   Parse
     { file = Nothing &= help "Parse the passed file.",
@@ -39,7 +37,7 @@ parse =
       sexp = False &= help "Print the parsed result as an s-expression."
     }
     &= details
-      [ "Parse a Remora program or expression.",
+      [ "Parse a remora program or expression.",
         "",
         "Expressions may be passed directly as an argument using the -e flag, e.g.:",
         "> remora parse -e \"[[1 2] [3 4]]\"",
@@ -47,20 +45,19 @@ parse =
         "If neither -f nor -e is passed, will read input from stdin."
       ]
 
+interpret :: RemoraMode
 interpret =
   Interpret
     { file = Nothing &= help "Interpret the passed file.",
       expr = Nothing &= help "Interpret an expression passed as an argument."
     }
     &= details
-      [ "Interpret a Remora program or expression.",
-        "",
-        "Expressions may be passed directly as an argument using the -e flag, e.g.:",
-        "> remora interpret -e \"[[1 2] [3 4]]\"",
+      [ "Interpret a remora program or expression.",
         "",
         "If neither -f nor -e is passed, will read input from stdin."
       ]
 
+mode :: Mode (CmdArgs RemoraMode)
 mode =
   cmdArgsMode $
     modes
@@ -72,8 +69,8 @@ mode =
 
 main :: IO ()
 main = do
-  mode <- cmdArgsRun mode
-  case mode of
+  passed_mode <- cmdArgsRun mode
+  case passed_mode of
     REPL -> CLI.REPL.repl
     Interpret mfile mexpr -> do
       input <- handleInput mfile mexpr
@@ -91,11 +88,10 @@ main = do
         Right expr
           | sexp -> T.putStrLn $ prettyText (toSExp expr :: SExp Text)
           | otherwise -> T.putStrLn $ prettyText expr
-    _ -> print "Unsupported."
   where
     handleInput :: Maybe FilePath -> Maybe String -> IO Text
     handleInput (Just path) _ = T.readFile path
     handleInput Nothing (Just s) = pure $ T.pack s
     handleInput Nothing Nothing = T.getContents
 
-    doParse mfile input = Parser.parse (fromMaybe "<cli>" mfile) input
+    doParse mfile = Parser.parse (fromMaybe "<cli>" mfile)
