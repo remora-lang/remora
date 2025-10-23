42cf5c1ffc7f809e7d5c4a5cdc6d5bb8882d57b4
Troels Henriksen <athas@sigkill.dk>
2025-10-23 20:05:20 +0200

    Generate IR, sort of.

--- src/Futhark.hs
+++ src/Futhark.hs
@@ -2,7 +2,9 @@ module Futhark (FutharkM, compile) where
 
 import Control.Monad.Error.Class
 import Control.Monad.Reader hiding (lift)
+import Control.Monad.State hiding (State)
 import Control.Monad.Trans.Except
+import Data.Bifunctor
 import Data.List qualified as L
 import Data.Text qualified as T
 import Prettyprinter
@@ -22,20 +24,6 @@ type Shape = Syntax.Shape VName
 
 type Type = Syntax.Type VName
 
-data Env = Env
-
-type Error = T.Text
-
--- | The Futhark generator monad.
-newtype FutharkM a = FutharkM {runFutharkM :: ExceptT Error (Reader Env) a}
-  deriving
-    ( Functor,
-      Applicative,
-      Monad,
-      MonadReader Env,
-      MonadError Error
-    )
-
 type FutharkExp = Doc ()
 
 type FutharkParam = Doc ()
@@ -46,11 +34,90 @@ type FutharkShape = Doc ()
 
 type FutharkDim = Doc ()
 
-apply :: FutharkExp -> FutharkExp -> FutharkExp
-apply f x = parens f <+> parens x
+type FutharkStm = Doc ()
+
+type FutharkStms = Doc ()
+
+type FutharkVar = Doc ()
+
+type FutharkFun = Doc ()
+
+type FutharkBody = Doc ()
+
+data Env = Env
+
+type Error = T.Text
+
+data State = State
+  { stateStms :: FutharkStms,
+    stateCounter :: Int,
+    stateFuns :: [FutharkFun]
+  }
+
+-- | The Futhark generator monad.
+newtype FutharkM a = FutharkM {runFutharkM :: StateT State (ReaderT Env (Except Error)) a}
+  deriving
+    ( Functor,
+      Applicative,
+      Monad,
+      MonadReader Env,
+      MonadError Error,
+      MonadState State
+    )
+
+emit :: FutharkStm -> FutharkM ()
+emit stm = modify $ \s -> s {stateStms = vsep [stateStms s, stm]}
+
+collect :: FutharkM a -> FutharkM (a, FutharkStms)
+collect m = do
+  old_stms <- gets stateStms
+  x <- m
+  stms <- gets stateStms
+  modify $ \s -> s {stateStms = old_stms}
+  pure (x, stms)
+
+newVar :: FutharkM FutharkVar
+newVar = do
+  x <- gets stateCounter
+  modify $ \s -> s {stateCounter = succ x}
+  pure $ "v_" <> pretty x
+
+bind :: FutharkType -> FutharkExp -> FutharkM FutharkVar
+bind t e = do
+  v <- newVar
+  emit $ "let" <+> braces (v <> ":" <+> t) <+> "=" <+> e
+  pure v
+
+mkBody :: FutharkM FutharkVar -> FutharkM FutharkBody
+mkBody m = do
+  (v, stms) <- collect m
+  pure $ vsep [stms, "in" <+> v]
+
+addFunction :: FutharkFun -> FutharkM ()
+addFunction fun = modify $ \s -> s {stateFuns = stateFuns s ++ [fun]}
+
+findRet :: Type -> Type
+findRet (_ :-> t) = findRet t
+findRet t = t
+
+-- Assumes the lambda has no free variables.
+liftLambda :: [(VName, Type)] -> Exp -> Type -> FutharkM FutharkVar
+liftLambda params body ret = do
+  params' <- mapM compileParam params
+  body' <- mkBody $ compileExp body
+  ret' <- compileType $ findRet ret
+  fname <- newVar
+  addFunction $
+    vsep
+      [ "fun" <+> fname <+> parens (mconcat $ punctuate ", " params'),
+        "  :" <+> braces ret' <+> "= {",
+        indent 2 body',
+        "}"
+      ]
+  pure fname
 
 compileDim :: Dim -> FutharkM FutharkDim
-compileDim (DimN x) = pure $ pretty x
+compileDim (DimN x) = pure $ pretty x <> "i64"
 compileDim d = error $ "compileDim: unhandled:\n" ++ show d
 
 compileShape :: Shape -> FutharkM FutharkShape
@@ -71,24 +138,18 @@ compileType t = error $ "compileType: unhandled:\n" ++ show t
 compileParam :: (VName, Type) -> FutharkM FutharkParam
 compileParam (v, t) = do
   t' <- compileType t
-  pure $ parens (pretty v <> ":" <+> t')
+  pure $ pretty v <> ":" <+> t'
 
 compileAtom :: Atom -> FutharkM FutharkExp
 compileAtom (Base (BoolVal True) _ _) = pure "true"
 compileAtom (Base (BoolVal False) _ _) = pure "false"
 compileAtom (Base (IntVal x) _ _) = pure $ pretty x <> "i32"
 compileAtom (Base (FloatVal x) _ _) = pure $ pretty x <> "f64"
-compileAtom (Lambda params body _ _) = do
-  params' <- mapM compileParam params
-  body' <- compileExp body
-  pure $
-    vcat
-      [ "\\" <> mconcat params' <+> "->",
-        indent 2 body'
-      ]
+compileAtom (Lambda params body (Typed t) _) = do
+  liftLambda params body t
 compileAtom e = error $ "compileAtom: unhandled:\n" ++ show e
 
-compileExp :: Exp -> FutharkM FutharkExp
+compileExp :: Exp -> FutharkM FutharkVar
 compileExp (Array [] [x] _ _) = compileAtom x
 compileExp (Array [_] elems _ _) = do
   elems' <- mapM compileAtom elems
@@ -98,18 +159,42 @@ compileExp (Var v _ _)
   | otherwise = pure $ parens $ pretty v
   where
     operators = ["+", "-"]
-compileExp (App f xs _ _) = do
+compileExp (App (Var v _ _) [x, y] (Typed (t, _)) _)
+  | Just v' <- lookup (varName v, t) binops = do
+      x' <- compileExp x
+      y' <- compileExp y
+      t' <- compileType t
+      bind t' $ v' <> parens (x' <> "," <> y')
+  where
+    binops =
+      [ (("+", TArr Int (Concat [])), "add32"),
+        (("-", TArr Int (Concat [])), "sub32")
+      ]
+compileExp (App f xs (Typed (t, _)) _) = do
   f' <- compileExp f
   xs' <- traverse compileExp xs
-  pure $ L.foldl' apply f' xs'
+  t' <- compileType t
+  bind t' $ "apply" <+> f' <+> hsep xs'
 compileExp e = error $ "compileExp: unhandled:\n" ++ show e
 
-wrapInMain :: FutharkExp -> T.Text
-wrapInMain e =
+wrapInMain :: (FutharkVar, State) -> T.Text
+wrapInMain (e, State stms _ funs) =
   renderStrict . layoutPretty defaultLayoutOptions $
-    vcat ["entry main () =", indent 2 e]
+    vcat $
+      funs
+        ++ [ "entry (\"main\", {}, {i32}) entry_main () : {i32} = {",
+             indent 2 (vcat [stms, "in" <+> e]),
+             "}"
+           ]
 
 -- | Turn Remora into Futhark.
 compile :: Prelude VName FutharkM -> Exp -> Either Error T.Text
 compile _prelude e =
-  wrapInMain <$> runReader (runExceptT (runFutharkM (compileExp e))) Env
+  wrapInMain
+    <$> runExcept
+      ( runReaderT
+          (runStateT (runFutharkM (compileExp e)) initialState)
+          Env
+      )
+  where
+    initialState = State mempty 0 mempty
