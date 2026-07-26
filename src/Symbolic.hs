44f3c6173e6e5d2e1c056d0aefac37fecda44538
zfnmxt <zfnmxt@zfnmxt.com>
2026-07-25 23:51:51 -0400

    Actually return 'Nothing' for shape subtraction failure. Closes #34.

--- src/Symbolic.hs
+++ src/Symbolic.hs
@@ -162,4 +162,4 @@ s \\ Concat [] = pure s
   | last ss @= last ts = Concat (init ss) \\ Concat (init ts)
 (Concat ss) \\ t
   | last ss @= t = pure $ Concat $ init ss
-s \\ t = error $ unlines ["No compatible p frame", prettyString s, prettyString t]
+_ \\ _ = Nothing
