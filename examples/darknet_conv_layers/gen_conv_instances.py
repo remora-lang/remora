3475c5acfd7b65ef3f193112b454d2a32ad94aec
Greg Sullivan <gsullivan@draper.com>
2026-08-17 11:30:06 -0400

    call conv-2d/rem in generated code

--- examples/darknet_conv_layers/gen_conv_instances.py
+++ examples/darknet_conv_layers/gen_conv_instances.py
@@ -16,10 +16,11 @@ def main() -> int:
     args = parser.parse_args()
 
     # copy polymorphic conv2d to stdout
-    with open("../darknet_conv.remora", 'r') as f:
+    with open("../darknet/conv_remainder.remora", 'r') as f:
+        print(";; start of conv_remainder.remora")
         for line in f:
             print(line, end='')
-        print(";; end of darknet_conv.remora")
+        print(";; end of conv_remainder.remora")
 
     with open(args.inFilePath, 'r') as f:
         _ignore = f.readline() # ignore 1st line
@@ -52,11 +53,11 @@ def main() -> int:
             #     : [Float {output_depth} {output_combined}]
             #   (@conv2d (Float) ({n_minus_k} {win_dim} {input_depth} {output_depth} 1) in w)))
             # '''
-            defn_str = f'''(entry (conv2d-rem/layer_{layer}
+            defn_str = f'''(entry (conv-2d-rem/layer_{layer}
                 (in [Float {input_depth} {input_dim} {input_dim}])
                 (w [Float {filters} {win_dim} {win_dim}])
                 : [Float {output_depth} {output_combined}])
-              (@conv2d/rem _ ({q} {win_dim} {input_depth} {output_depth} {pad} {stride} {r}) in w))
+              (@conv-2d/rem _ ({q} {win_dim} {input_depth} {output_depth} {pad} {stride} {r}) in w))
             '''
             print(defn_str)
     return 0
