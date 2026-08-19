811cd9546e1aa2c82b981350c595e12056cb9691
Sarah-Scott <sarah.johnson@kestrel.edu>
2026-08-19 14:15:09 -0500

    serialize futhark passes

--- src/Pipeline.hs
+++ src/Pipeline.hs
@@ -11,12 +11,16 @@ module Pipeline
     defunctionalizeExp,
     compile,
     compileExp,
+    PassAST (..),
+    compileWithASTs,
+    compileWithASTsExp,
     interpret,
     interpretExp,
   )
 where
 
 import Control.Monad
+import Data.Aeson (ToJSON, Value, toJSON)
 import Data.Text (Text)
 import Defunctionalize qualified
 import Futhark qualified
@@ -24,6 +28,7 @@ import Interpreter qualified
 import LambdaLift qualified
 import Monomorphize qualified
 import Pass
+import Serializer ()
 import Syntax
 import TypeCheck qualified
 import Uniquify qualified
@@ -118,22 +123,64 @@ compileExp :: UncheckedExp -> Either Error Text
 compileExp = runPass . compileExpM
 
 compileM :: UncheckedProg -> PassM Text
-compileM =
-  Uniquify.uniquify
-    >=> TypeCheck.check
-    >=> LambdaLift.lambdaLift
-    >=> Monomorphize.monomorphize
-    >=> Defunctionalize.defunctionalize
-    >=> Futhark.compile
+compileM = fmap snd . compileWithASTsM
 
 compileExpM :: UncheckedExp -> PassM Text
-compileExpM =
-  Uniquify.uniquifyExp
-    >=> TypeCheck.checkExp
-    >=> LambdaLift.lambdaLiftExp
-    >=> Monomorphize.monomorphizeExp
-    >=> Defunctionalize.defunctionalizeExp
-    >=> Futhark.compileExp
+compileExpM = fmap snd . compileWithASTsExpM
+
+-- | The AST produced by one pass of the compilation pipeline,
+-- serialized to JSON, and the name of the corresponding pass.
+data PassAST = PassAST
+  { passName :: String,
+    passJSON :: Value
+  }
+
+mkPassAST :: (ToJSON a) => String -> a -> PassAST
+mkPassAST name = PassAST name . toJSON
+
+compileWithASTs :: UncheckedProg -> Either Error ([PassAST], Text)
+compileWithASTs = runPass . compileWithASTsM
+
+compileWithASTsExp :: UncheckedExp -> Either Error ([PassAST], Text)
+compileWithASTsExp = runPass . compileWithASTsExpM
+
+compileWithASTsM :: UncheckedProg -> PassM ([PassAST], Text)
+compileWithASTsM parsed = do
+  uniquified <- Uniquify.uniquify parsed
+  checked <- TypeCheck.check uniquified
+  lifted <- LambdaLift.lambdaLift checked
+  monomorphized <- Monomorphize.monomorphize lifted
+  defunctionalized <- Defunctionalize.defunctionalize monomorphized
+  ir <- Futhark.compile defunctionalized
+  pure
+    ( [ mkPassAST "parse" parsed,
+        mkPassAST "uniquify" uniquified,
+        mkPassAST "typecheck" checked,
+        mkPassAST "lambdalift" lifted,
+        mkPassAST "monomorphize" monomorphized,
+        mkPassAST "defunctionalize" defunctionalized
+      ],
+      ir
+    )
+
+compileWithASTsExpM :: UncheckedExp -> PassM ([PassAST], Text)
+compileWithASTsExpM parsed = do
+  uniquified <- Uniquify.uniquifyExp parsed
+  checked <- TypeCheck.checkExp uniquified
+  lifted <- LambdaLift.lambdaLiftExp checked
+  monomorphized <- Monomorphize.monomorphizeExp lifted
+  defunctionalized <- Defunctionalize.defunctionalizeExp monomorphized
+  ir <- Futhark.compileExp defunctionalized
+  pure
+    ( [ mkPassAST "parse" parsed,
+        mkPassAST "uniquify" uniquified,
+        mkPassAST "typecheck" checked,
+        mkPassAST "lambdalift" lifted,
+        mkPassAST "monomorphize" monomorphized,
+        mkPassAST "defunctionalize" defunctionalized
+      ],
+      ir
+    )
 
 interpret :: Text -> [Interpreter.Val] -> UncheckedProg -> Either Error Interpreter.Val
 interpret entry args = runPass . interpretM entry args
